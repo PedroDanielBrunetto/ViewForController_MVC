fdeae3c029ac019928f09abf8e4d789e9c698367
pedrodanielbrunetto <pedrodanielbm@hotmail.com>
2024-03-14 19:51:05 -0300

    first commit

---
+++ simulador_proposta/Controllers/SimulacaoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using simulador_proposta.Models;
+
+namespace simulador_proposta.Controllers
+{
+    public class SimulacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new SimulacaoModel();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SimulacaoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Create", model);
+            }
+
+            return View("Index", model);
+        }
+
+    }
+}
